22504323c338c48f2cd936f568384b3783fe5d83
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2022-07-08 18:37:24 +0200

    wip

--- src/PriorityServiceProvider.php
+++ src/PriorityServiceProvider.php
@@ -13,11 +13,18 @@ class PriorityServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Register plugin
-        Taskday::register(new PriorityPlugin, 'priority');
-
         // Registering extra web routes
         Route::middleware('web')->group(__DIR__ . '/../routes/web.php');
+    
+        if (Taskday::getViewByType('kanban')) {
+            Taskday::getViewByType('kanban')->add('priority', [
+                [ "value" => 0, "name" => 'No priority', "color" => 'gray'   ],
+                [ "value" => 1, "name" => 'Low'        , "color" => 'blue'   ],
+                [ "value" => 2, "name" => 'Medium'     , "color" => 'yellow' ],
+                [ "value" => 3, "name" => 'High'       , "color" => 'orange' ],
+                [ "value" => 4, "name" => 'Urgent!'    , "color" => 'red'    ],
+            ]);
+        }
     }
 
     /**
@@ -25,6 +32,7 @@ class PriorityServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Register plugin
+        Taskday::register(new PriorityPlugin, 'priority');
     }
 }
